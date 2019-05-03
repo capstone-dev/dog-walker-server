8ee87f42fb1dbb428c9dc363ce0a8affcb9f264a
Seongweon Seo <kissshot93@gmail.com>
2019-05-03 19:00:12 +0900

    [appserver]

---
+++ app-server/routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+/* GET home page. */
+/*
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+*/
+
+// DATABASE SETTING
+var connection=require('./dbConnection');
+router.get('/',function(req, res, next) {
+
+var query=connection.query('select * from user',
+function(err,rows){
+console.log("here1");
+//if(err)throw err;
+if(rows[0]){
+res.json(rows[0]);
+console.log("here2");
+console.log(rows[0])
+//console.log(rows[0])
+
+//responseData.result="ok";
+//responseData.name=rows[0].name;
+}else{
+console.log("no rows")
+//responseData.result="none";
+//responseData.name="";
+}
+})
+});
+
+
+
+
+
+
+module.exports = router;
