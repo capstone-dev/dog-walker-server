b817866d0a3fe01bc7ef47ef9f5bd89d6f6eacd1
xuti <rehabilitationz@naver.com>
2019-05-09 20:24:41 +0900

    signUp.js 추가

--- app-server/app.js
+++ app-server/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var threadRouter=require('./routes/thread');
 var commentRouter=require('./routes/comment');
+var signUpRouter=require('./routes/signUp');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/thread',threadRouter);
 app.use('/comment',commentRouter);
+app.use('/signUp',signUpRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
