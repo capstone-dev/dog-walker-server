0d4cc5828a2d1bc1e3367cd297b74de57aa15e67
Ubuntu <ubuntu@ip-172-31-26-150.ap-northeast-2.compute.internal>
2019-05-10 21:29:28 +0900

    메인 페이지4

--- app-server/routes/index.js
+++ app-server/routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path=require('path');
 
 //LOGGER SETTING
 const logger=require('../configurations/logConfiguration');
 
 router.get('/',function(req, res, next) {
-    res.sendFile(__dirname+"../views/main.html")
+    res.sendFile(path.resolve('views/main.html'))
 });
 
 module.exports = router;
