60e036d0400f9ad24d588372e711114ad6fd9c41
xuti <xutistory@gmail.com>
2019-05-10 19:50:54 +0900

    로그 관련 디펜던시 추가2

--- app-server/app.js
+++ app-server/app.js
@@ -13,10 +13,6 @@ var app = express();
 //LOGGER SETTING
 const logger=require('./configurations/logConfiguration');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
